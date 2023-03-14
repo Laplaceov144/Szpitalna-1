2377345a54a8edd4e8d843e54411a88124227598
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2023-03-14 12:29:58 +0100

    Update residents.js

--- residents.js
+++ residents.js
@@ -19,7 +19,7 @@ const residents = (arr) => {
 // Displaying residents' info
 const displayResidents = (arr) => {
     
-    const residents = dynamicTile(arr);
+    const residents = dynamicTile(arr).join(' ');
 
   container.innerHTML = `${residents}`;
 
