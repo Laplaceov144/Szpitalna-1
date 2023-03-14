1728d1a7f7437977740fe32ffe8e34609d46f32a
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2023-03-14 20:42:59 +0100

    Update event.js

--- event.js
+++ event.js
@@ -1,10 +1,7 @@
-import selector from '.utils/functions.js';
-import dynamicTile from '.utils/functions.js';
-
 const url = './utils/database.json';
-const eventDOM = selector('.event');
-const tabTitle = selector('title');
-const subHall = selector('.sub-hall');
+const eventDOM = document.querySelector('.event');
+const tabTitle = document.querySelector('title');
+const subHall = document.querySelector('.sub-hall');
 
 // Fetching data from database.json
 let thisEvent;
@@ -31,7 +28,23 @@ const displayEvent = (event) => {
   tabTitle.innerText = title;
   const description = event.description;
   const lineup = event.lineup;
-  const artistsList = dynamicTile(lineup).join(' ');
+  const artistsList = lineup.map((item) => {
+    
+    return `<li><a class="artist-img" href="${item.img}" target="_blank"><img src="${item.img}"></a>
+    <a class="artist-nick">${item.nickname}</a>
+    <div class="artist-info"><p class="small-bio">${item.bio} </p>
+    <div class="links">
+    <a target="_blank" class="link" href="${item.sc}"><img class="icon"  src="${item.SCicon}" 
+    alt="" onerror='this.remove()'></a>
+    <a target="_blank" class="link" href="${item.fb}"><img class="fb-icon"  src="${item.FBicon}" 
+    alt="" onerror='this.remove()'></a>
+    <a target="_blank" class="link" href="${item.ig}"><img class="icon"  src="${item.IGicon}" 
+    alt="" onerror='this.remove()'></a>
+    <a target="_blank" class="link" href="${item.ra}"><img class="icon"  src="${item.RAicon}" 
+    alt="" onerror='this.remove()'></a>
+    </div></div>
+    </li>`;
+  }).join(' ');
   
   title.innerText = `${title}`;
   eventDOM.innerHTML = `<div class="event-wrapper">
