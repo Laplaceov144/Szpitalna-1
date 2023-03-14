dba59fa81f1fa16ad07b02b4176ba19dcf0e727a
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2023-03-14 11:37:17 +0100

    Update functions.js

--- utils/functions.js
+++ utils/functions.js
@@ -11,18 +11,20 @@ export const fetchData = = async () => {
   }
 };
 
-export const dynamicTile(item) = `<li><a class="artist-img" href="${item.img}" target="_blank"><img src="${item.img}"></a>
-            <a class="artist-nick">${item.nickname}</a>
-            <div class="artist-info"><p class="small-bio">${item.bio} </p>
-            <div class="links">
-            <a target="_blank" class="link" href="${item.sc}"><img class="icon"  src="${item.SCicon}" 
-            alt="" onerror='this.remove()'></a>
-            <a target="_blank" class="link" href="${item.fb}"><img class="fb-icon"  src="${item.FBicon}" 
-            alt="" onerror='this.remove()'></a>
-            <a target="_blank" class="link" href="${item.ig}"><img class="icon"  src="${item.IGicon}" 
-            alt="" onerror='this.remove()'></a>
-            <a target="_blank" class="link" href="${item.ra}"><img class="icon"  src="${item.RAicon}" 
-            alt="" onerror='this.remove()'></a>
-            </div></div>
-            </li>`;
+export const dynamicTile(item) = lineup.map((item) => {
+    
+    return `<li><a class="artist-img" href="${item.img}" target="_blank"><img src="${item.img}"></a>
+    <a class="artist-nick">${item.nickname}</a>
+    <div class="artist-info"><p class="small-bio">${item.bio} </p>
+    <div class="links">
+    <a target="_blank" class="link" href="${item.sc}"><img class="icon"  src="${item.SCicon}" 
+    alt="" onerror='this.remove()'></a>
+    <a target="_blank" class="link" href="${item.fb}"><img class="fb-icon"  src="${item.FBicon}" 
+    alt="" onerror='this.remove()'></a>
+    <a target="_blank" class="link" href="${item.ig}"><img class="icon"  src="${item.IGicon}" 
+    alt="" onerror='this.remove()'></a>
+    <a target="_blank" class="link" href="${item.ra}"><img class="icon"  src="${item.RAicon}" 
+    alt="" onerror='this.remove()'></a>
+    </div></div>
+    </li>`;
   }).join(' ');
