e2b7d5561d7171dbb1ffaee1148df58f95b04a07
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2023-03-14 11:29:30 +0100

    Update residents.js

--- residents.js
+++ residents.js
@@ -1,17 +1,7 @@
-const url = './utils/database.json';
-const container = document.querySelector('.residents-container');
+import selector, fetchData, dynamicTile from '.utils/functions,js';
 
-// Fetching data from database.json
-const fetchArtists = async () => {
-    container.innerHTML = '<div class="loading"></div>';
-    try {
-      const resp = await fetch(url);
-      const data = await resp.json();
-      return data;
-    } catch (error) {
-      container.innerHTML = '<p class="error">there was an error</p>';
-    }
-};
+const url = './utils/database.json';
+const container = selector('.residents-container');
 
 // Filtering resident DJs
 const residents = (arr) => {
@@ -28,22 +18,7 @@ const residents = (arr) => {
 const displayResidents = (arr) => {
 
     const residents = arr.map((item) => {
-
-    return `<li><a class="artist-img" href="${item.img}" target="_blank"><img src="${item.img}"></a>
-            <a class="artist-nick">${item.nickname}</a>
-            <div class="artist-info"><p class="small-bio">${item.bio} </p>
-            <div class="links">
-            <a target="_blank" class="link" href="${item.sc}"><img class="icon"  src="${item.SCicon}" 
-            alt="" onerror='this.remove()'></a>
-            <a target="_blank" class="link" href="${item.fb}"><img class="fb-icon"  src="${item.FBicon}" 
-            alt="" onerror='this.remove()'></a>
-            <a target="_blank" class="link" href="${item.ig}"><img class="icon"  src="${item.IGicon}" 
-            alt="" onerror='this.remove()'></a>
-            <a target="_blank" class="link" href="${item.ra}"><img class="icon"  src="${item.RAicon}" 
-            alt="" onerror='this.remove()'></a>
-            </div></div>
-            </li>`;
-  }).join(' ');
+        return dynamicTile(item);
 
   container.innerHTML = `${residents}`;
 
@@ -51,7 +26,7 @@ const displayResidents = (arr) => {
 
 // Inserting everything dynamically in 'residents.html'
 const start = async () => {
-    const data = await fetchArtists();
+    const data = await fetchData();
     displayResidents(residents(data));    
 };
   start();
