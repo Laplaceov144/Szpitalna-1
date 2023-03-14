f6374bd503b3f169756eb8c58bd653c30c8f17ba
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2023-03-14 12:34:35 +0100

    Update residents.js

--- residents.js
+++ residents.js
@@ -1,5 +1,4 @@
 import selector from '.utils/functions.js';
-import dynamicTile from '.utils/functions.js';
 
 const url = './utils/database.json';
 const container = selector('.residents-container');
@@ -30,7 +29,23 @@ const residents = (arr) => {
 // Displaying residents' info
 const displayResidents = (arr) => {
     
-    const residents = dynamicTile(arr).join(' ');
+    const residents = arr.map((item) => {
+    
+    return `<li><a class="artist-img" href="${item.img}" target="_blank"><img src="${item.img}"></a>
+    <a class="artist-nick">${item.nickname}</a>
+    <div class="artist-info"><p class="small-bio">${item.bio} </p>
+    <div class="links">
+    <a target="_blank" class="link" href="${item.sc}"><img class="icon"  src="${item.SCicon}" 
+    alt="" onerror='this.remove()'></a>
+    <a target="_blank" class="link" href="${item.fb}"><img class="fb-icon"  src="${item.FBicon}" 
+    alt="" onerror='this.remove()'></a>
+    <a target="_blank" class="link" href="${item.ig}"><img class="icon"  src="${item.IGicon}" 
+    alt="" onerror='this.remove()'></a>
+    <a target="_blank" class="link" href="${item.ra}"><img class="icon"  src="${item.RAicon}" 
+    alt="" onerror='this.remove()'></a>
+    </div></div>
+    </li>`;
+  }).join(' ');
 
   container.innerHTML = `${residents}`;
 
