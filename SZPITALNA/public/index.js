8f2e7c70d40ae16d070952e068394657dc6d7b89
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2023-03-14 06:21:14 +0100

    Update index.js

--- SZPITALNA/public/index.js
+++ SZPITALNA/public/index.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.events-center');
-const url = './utils/database.json';
+const url = '.public/utils/database.json';
 const hall = document.querySelector('.hall');
 const form = document.querySelector('.input-form');
 const searchInput = document.querySelector('.search-input');
@@ -24,7 +24,7 @@ const displayEvents = (item) => {
     const weekday = event.weekday;
     const month = event.month;
     const title = event.title;   
-    return `<a class="single-event" href="event.html?id=${day}">
+    return `<a class="single-event" href="./public/event.html?id=${day}">
               <div>
                 <h4 class="event-date">${weekday}, ${day}/${month}</h4>
                 <h2>${title}</h2>
