5d92b751fcb7c5b0493796016d0fe1f87070e4b3
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2023-03-14 11:24:02 +0100

    Create functions.js

---
+++ utils/functions.js
@@ -0,0 +1,28 @@
+export const selector(qs) = document.querySelector(qs);
+
+export const fetchData = = async () => {
+  container.innerHTML = '<div class="loading"></div>';
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    container.innerHTML = '<p class="error">there was an error</p>';
+  }
+};
+
+export const dynamicTile = `<li><a class="artist-img" href="${item.img}" target="_blank"><img src="${item.img}"></a>
+            <a class="artist-nick">${item.nickname}</a>
+            <div class="artist-info"><p class="small-bio">${item.bio} </p>
+            <div class="links">
+            <a target="_blank" class="link" href="${item.sc}"><img class="icon"  src="${item.SCicon}" 
+            alt="" onerror='this.remove()'></a>
+            <a target="_blank" class="link" href="${item.fb}"><img class="fb-icon"  src="${item.FBicon}" 
+            alt="" onerror='this.remove()'></a>
+            <a target="_blank" class="link" href="${item.ig}"><img class="icon"  src="${item.IGicon}" 
+            alt="" onerror='this.remove()'></a>
+            <a target="_blank" class="link" href="${item.ra}"><img class="icon"  src="${item.RAicon}" 
+            alt="" onerror='this.remove()'></a>
+            </div></div>
+            </li>`;
+  }).join(' ');
